b879e0b210d12371ec36460261c20e24f98f0dd4
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-09-10 15:40:33 +0530

    changes

--- Casio Calculator/index.js
+++ Casio Calculator/index.js
@@ -48,50 +48,5 @@
         }
     });
 
-    // keyboard events
-
-// window.addEventListener("keydown", (e) => {
-//     if (
-//       e.key === "0" ||
-//       e.key === "1" ||
-//       e.key === "2" ||
-//       e.key === "3" ||
-//       e.key === "4" ||
-//       e.key === "5" ||
-//       e.key === "6" ||
-//       e.key === "7" ||
-//       e.key === "8" ||
-//       e.key === "9" ||
-//       e.key === "." 
-//     ) {
-//       clickButtonEl(e.key);
-//       // console.log(e.key)
-//     } else if (e.key === "+" || e.key === "-" || e.key === "/") {
-//       clickOperation(e.key);
-//     } else if (e.key === "*") {
-//       clickOperation("x");
-//       // console.log(e.key)
-//     } else if (e.key == "Enter" || e.key === "=") {
-//       clickEqual();
-//     }
-//     // console.log(e.key)
-//   });
-//   function clickButtonEl(key) {
-//     numbersEl.forEach((button) => {
-//       if (button.value === key) {
-//         button.click();
-//       }
-//     });
-//   }
-//   function clickOperation(key) {
-//     operationEl.forEach((operator) => {
-//       if (operator.value === key) {
-//         operator.click();
-//       }
-//     });
-//   }
-//   function clickEqual() {
-//     equal.click();
-//   }
-
+    
 })();
